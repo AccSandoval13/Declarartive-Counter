4b8546729a37b7e132d2cdb6175ec4ff7fc76b9b
Ricardo Sandovale <AccSandoval13@gmail.comm>
2023-03-06 17:29:19 -0800

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+// import React from 'react'
+// import { useState } from 'react'
 
+// or with one line below 
+import React, { useState } from 'react'
+
+// invoked the useState function, with two returned variables
+// you can interpolate JavaScript into JSX using {}
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  let [counter, changeCounter] = useState(1)
+  // To access this function in the developer console, we need to make it global
+  window.changeCounter = changeCounter 
+    return (
+      <div>
+          <h1>{counter}</h1>
+          <h4>{counter}</h4>
+      </div>
+    )
 }
+// React automatically updated the DOM in multiple places when necessary
 
 export default App;
+
